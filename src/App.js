2b91b55a09cc2404b25a0bae28ad2f7f880e0ba4
Ajay Bidyarthy <47785474+AjayBidyarthy@users.noreply.github.com>
2024-10-28 15:51:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,80 +1,10 @@
 import React, { Suspense, useContext, lazy, useEffect } from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard';
-import Competitors from './pages/Competitors';
-import DashboardHome from './components/Dashboard/DashboardHome';
-import ViewAll from './components/Dashboard/ViewAll';
-import CompetitorHome from './components/Competitors/CompetitorHome';
-import ResearchHome from './components/ResearchTab/ResearchHome';
-import OrganicKeyword from './components/ResearchTab/OrganicPaidKeywords/OrganicKeyword';
-import PaidKeyword from './components/ResearchTab/OrganicPaidKeywords/PaidKeyword';
-import PaidCompetitors from './components/ResearchTab/PaidCompetitors';
-import Home from './components/InventoryManagement/Home';
-import OrderListPage from './components/InventoryManagement/OrderListPage';
-import MonitoredURL from './components/Competitors/MonitoredURL';
-import MapInfragment from './components/Competitors/MapInfragment';
-import RepricingHome from './pages/RepricingHome';
-import RepricingRules from './components/Repricing/RepricingRules';
-import RepricingOverview from './components/Repricing/RepricingOverview';
+
 import RepricingHistory from './components/Repricing/RepricingHistory';
 import ReportHome from './components/Report/ReportHome';
 import MyCatalogs from './pages/MyCatalogs';
-import SettingHome from './components/Settings/SettingHome';
-import AddNew from './pages/AddNew';
-import OrganicCompetitors from './components/ResearchTab/OrganicCompetitors';
-import MyProduct from './pages/MyProduct';
-import AlertHome from './pages/AlertHome';
-import AlertRules from './components/Alert/AlertRules';
-import PriceAlert from './components/Alert/PriceAlert';
-import InventoryAlert from './components/Alert/InventoryAlert';
-import ResearchAlert from './components/Alert/ResearchAlert';
-import Apps from './components/Apps/Apps';
-import SettingsGeneral from './components/Settings/SettingsGeneral';
-import SettingsCommPrefrence from './components/Settings/SettingsCommPrefrence';
-import SettingsAPIKeys from './components/Settings/SettingsAPIKeys';
-import ManageUsersHome from './components/ManageUsers/ManageUsersHome';
-import Profiles from './components/ManageUsers/Profiles';
-import ChangePassword from './components/ManageUsers/ChangePassword';
-import Users from './components/ManageUsers/Users';
-import Admin from './components/ManageUsers/Admin';
-import HelpCenter from './components/HelpCenter/HelpCenter';
-import PaymentHome from './components/Payment/PaymentHome';
-import PaymentBilling from './components/Payment/PaymentBilling';
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import 'react-date-picker/dist/DatePicker.css';
-import 'react-calendar/dist/Calendar.css';
-import PaymentPlans from './components/Payment/PaymentPlans';
-import SuggestFeature from './components/SuggestFeature/SuggestFeature';
-import Feedback from './components/SuggestFeature/Feedback';
-import Roadmap from './components/SuggestFeature/Roadmap';
-import Keyword from './components/ResearchTab/Keyword/Keyword';
-import ViewAllTable from './components/InventoryManagement/ViewAllTable'
-import AddNewReport from './components/Report/AddNewReport';
-import ReportLayout from './components/Report/ReportLayout'
-import CreateEditRule from './components/Repricing/CreateEditRule'
-import Clicks from './components/ResearchTab/Clicks';
-import Change from './components/ResearchTab/Change';
-import OwnerDashboard from './components/Superuser/OwnerDashboard'
-import AdminDashboard from './components/Admin/AdminDashboard';
-import EditUser from './components/Superuser/EditUser';
-import AdminEditUser from './components/Admin/AdminEditUser';
-import AdminAddUser from './components/Admin/AdminAddUser';
-import UserAccess from './components/Superuser/UserAccess';
-import Organization from './pages/Organization';
-
-import { NextUIProvider } from "@nextui-org/react";
-import AddNewMarketplace from './components/Competitors/AddNewMarketplace';
-import InProgress from './components/SuggestFeature/InProgress';
-import FeedbackFeedback from './components/SuggestFeature/FeedbackFeedback';
-import OwnerAddUser from './components/Superuser/OwnerAddUser';
-// import { getUserData } from './utility/Utils';
-
-function App() {
-
-  // const user = getUserData();
 
   return (
     <>
