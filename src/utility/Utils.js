66c91170d67fed98267da60eb8e20af60a6db27b
Ajay Bidyarthy <47785474+AjayBidyarthy@users.noreply.github.com>
2024-10-28 15:52:45 +0530

    Update Utils.js

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -1,21 +1,3 @@
 import jwt from 'jsonwebtoken'
 
-export const isUserLoggedIn = () => {
-    const data = JSON.parse(localStorage.getItem('userData'))
-    if (data) {
-        const secrect = jwt.decode(data.token)
-        const expiretime = secrect.exp * 1000
-        if (Date.now() >= expiretime) {
-            // logoutApi(data);
-            localStorage.removeItem('userData')
-            // return false
-        }
-        //   else {
-        //     console.log('skdcbwjecbkwjecnwe', Date.now() >= expiretime);
-        //     return true
-        //   }
-    }
-    return JSON.parse(localStorage.getItem('userData'))
-}
-
-export const getUserData = () => JSON.parse(localStorage.getItem('userData'))
+export lStorage.getItem('userData'))
