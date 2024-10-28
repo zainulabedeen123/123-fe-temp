8c141010d3c408c0b12d527b120bd8d028e69654
Ajay Bidyarthy <47785474+AjayBidyarthy@users.noreply.github.com>
2024-10-28 15:53:39 +0530

    Update Organization.js

--- src/pages/Organization.js
+++ src/pages/Organization.js
@@ -1,16 +1,3 @@
-import React from 'react'
-import { Outlet, useLocation, useNavigate, useOutletContext } from 'react-router-dom';
-
-const Organization = () => {
-    const { userRole } = useOutletContext();
-
-    const navigate = useNavigate();
-
-    const pathname = useLocation().pathname.split('/')[2];
-
-
-    return (
-        <>
 
             <div className=" flex space-x-5 px-2 mb-8">
                 {userRole === 'owner' && (
@@ -19,17 +6,4 @@ const Organization = () => {
                     </p>
                 )}
                 {userRole === 'admin' && (
-                    <p onClick={() => navigate('/organization/admin/dashboard')} className={`${pathname === 'admin' && 'text-white'} cursor-pointer`}>
-                        Admin
-                    </p>
-                )}
-            </div>
-
-            <Outlet />
-
-        </>
-    )
-}
-
-
 export default Organization
